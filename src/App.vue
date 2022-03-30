4953df1829516df2bb0e23a24e287ffec518a21d
alessandrojuza <mascia.alessandro89@gmail.com>
2022-03-30 11:23:55 +0200

    Basic Layout structure

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,138 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main-container">
+    <div class="search-bar-container">
+      <img src="./assets/img/logo.png" alt="logo" class="logo" />
+      <div class="search-bar">
+        <input
+          type="text"
+          value=""
+          class="search-bar-input"
+          placeholder="Enter Your Favorite Cocktail:"
+        />
+        <CloseIcon class="close-icon" onClick="{}" />
+      </div>
+    </div>
+  </div>
+
+  <div className="result-container"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import CocktailResult from "./components/CocktailResult.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CocktailResult,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
+  box-sizing: border-box;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-image: url(../src/assets/img/background.jpg);
+  background-size: cover;
+
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: #f1faee;
+}
+
+.main-container {
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  height: 900px;
+  width: 900px;
+
+  padding: 40px;
+
+  background: rgba(0, 0, 0, 0.596);
+  border-radius: 20px;
+  backdrop-filter: blur(10px);
+}
+
+.search-bar-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  border-bottom: 2px solid $transparent-white;
+}
+
+.logo {
+  display: flex;
+
+  height: 35px;
+  margin-bottom: 40px;
+}
+
+.search-bar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 60px;
+  width: 600px;
+
+  background: rgba(255, 255, 255, 0.082);
+
+  border-radius: 10px;
+  margin-bottom: 40px;
+
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+
+.search-bar-input {
+  height: 50px;
+  width: 500px;
+
+  text-align: center;
+  font-size: 16pt;
+
+  color: white;
+  background: transparent;
+  border: none;
+}
+
+.search-bar-input:focus {
+  outline: none;
+}
+
+.search-bar-input:focus::placeholder {
+  color: transparent;
+  font-size: 10pt;
+}
+
+.close-icon {
+  transition: color 0.5s;
+}
+
+.close-icon:hover {
+  cursor: pointer;
+  color: $transparent-white;
+}
+
+.result-container {
+  overflow-y: scroll;
+  padding-top: 0px;
 }
 </style>
---
+++ src/components/CocktailResult.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <p>Sono un component importato</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CocktailResult",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
